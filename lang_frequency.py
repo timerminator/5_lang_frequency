e53ee574edbbc31747cab6f6b3b84677d40de700
timerminator <timerbaevruslan@gmail.com>
2018-05-10 18:04:54 +0500

    Add files via upload
    the basic functionality is described

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,33 @@
-
+import re
+import sys
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file:
+        return file.readlines()
 
 
 def get_most_frequent_words(text):
-    pass
+    words = []
+    for line in text:
+        words += re.findall('\w+', line)
+
+    frequency_of_words = {}
+
+    for word in words:
+        if word not in frequency_of_words:
+            frequency_of_words[word] = 1
+        else:
+            frequency_of_words[word] += 1
+
+        top10_frequency_of_words = sorted(frequency_of_words.items(), key=lambda x: x[1],reverse=True)[:10]
+    for word in top10_frequency_of_words:
+        print('Слово "%s" встречается в тексте %s раз.'% ( word[0], word[1]))
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        get_most_frequent_words(load_data(sys.argv[1]))
+    except IndexError as e:
+        print('Вы не указали путь к файлу')
+    except IOError as e:
+        print('Не удалось открыть файл')
